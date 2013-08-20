8a800c4743214071e551aab5e2753352987cff51
Louis Szgalsky <lszgalsky13@gmail.com>
2013-08-20 12:58:42 -0400

    Fixed broken pipe issue on client side

--- sender.cpp
+++ sender.cpp
@@ -198,7 +198,10 @@ int main(int argc, char* argv[])
 
 	    	// close socket on keypress
 		if(press->detail == 2)
+		{
+			xcb_flush(display);
 			socket.close();
+		}
 	    	break;
 	    	
 	    	
