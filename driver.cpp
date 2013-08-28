a54b896406244b2cd5a38252ca7f18e19887170a
OB <oodabaga@gmail.com>
2013-08-28 10:01:29 -0400

    Fixed issue where driver.cpp was not push to repository

---
+++ driver.cpp
@@ -0,0 +1,24 @@
+#include "xcbutil.h"
+#include "vcursor.h" 
+#include <unistd.h>
+#include <stdio.h>
+#include <xcb/xcb.h>
+using namespace std;
+int main ( ) {
+	xcbInit();	//initialize xcb connection
+	Vcursor * temp;
+	Vcursor * temp2;
+	temp = Vcursor::getCursor();
+	temp2 = Vcursor::getCursor();
+	temp->show();
+	temp2->show();
+	xcb_unmap_window( display, temp->getWindowId() );
+	xcb_flush(display);
+//	temp2->hide();
+	printf("%d\n", temp->getMouseId());
+	printf("%d\n", temp2->getMouseId());
+	xcb_flush ( display );	//send all commands before we pause so window gets shown
+	pause ( );	//hold until ctrl-c
+	xcb_disconnect(display);
+	return 0;
+}
